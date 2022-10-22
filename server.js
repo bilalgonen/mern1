91242ecece23360cbe7767b3c108baabfba84983
Bilal Gonen <bilalgonen@Bilals-Air.attlocal.net>
2022-10-22 08:15:37 -0400

    Connected to DB

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const cookieParser = require("cookie-parser");
 // const path = require("path");
 
@@ -28,20 +28,16 @@ app.get("/", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 // });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database");
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("Connected to database");
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server running on port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
